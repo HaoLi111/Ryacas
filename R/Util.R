54eaf1b27fda23fd320494c04fe572c0464ea01f
Hao <32727671+HaoLi111@users.noreply.github.com>
2018-06-04 13:59:59 +1200

    Add files via upload

--- R/Util.R
+++ R/Util.R
@@ -4,21 +4,6 @@ PrettyForm.function<-function(func){
 
 TeXForm.function<-function(func) TeXForm(as.expression(body(func)))
 
-
-Sym<-function (...){
-  if(!is.character(...)|missing(...)){
-    assign(deparse(substitute(...)),Sym(paste(as.character(deparse(substitute(...))),'s',sep = '')),pos = .GlobalEnv)
-  }else{
-    args <- list(...)
-    value <- if (length(args) > 1)
-      paste("(", ..., ")")
-    else paste(args[[1]])
-    class(value) <- c("Sym", "character")
-    value
-  }
-}
-
-
 RForm<-function(y) UseMethod('RForm')
 RForm.yacas<-function(y) return(parse(text=y[["text"]]))
 
